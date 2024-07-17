b78da9d0d3fa4b42498ba761ee4948171bfe3ae5
Zhiquan Lao <archer55190360@gmail.zom>
2024-07-17 00:15:41 -0400

    Finish CS61B Lab1

---
+++ lab1/HelloNumber.java
@@ -0,0 +1,9 @@
+public class HelloNumber{
+    public static void main(String[] args){
+        int sum=0;
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(sum);
+            sum+=i;
+        }
+    }
+}
