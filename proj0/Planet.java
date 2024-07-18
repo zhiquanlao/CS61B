6974058804886587f87f5323def4a04b13c278f8
Zhiquan Lao <archer55190360@gmail.zom>
2024-07-17 23:39:51 -0400

    Finish Calculating Distance

---
+++ proj0/Planet.java
@@ -0,0 +1,29 @@
+public class Planet{
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+    public Planet(Planet p){
+        xxPos=p.xxPos;
+        yyPos=p.yyPos;
+        xxVel=p.xxVel;
+        yyVel=p.yyVel;
+        mass=p.mass;
+        imgFileName=p.imgFileName;
+    }
+    public Planet(double xP, double yP, 
+    double xV, double yV, double m, String img){
+        xxPos=xP;
+        yyPos=yP;
+        xxVel=xV;
+        yyVel=yV;
+        mass=m;
+        imgFileName=img;
+    }
+    public double calcDistance(Planet p){
+        return Math.sqrt((xxPos-p.xxPos)*(xxPos-p.xxPos)
+        +(yyPos-p.yyPos)*(yyPos-p.yyPos));
+    }
+}
