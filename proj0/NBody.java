cd37b94e96d78c2e418bd91ad591a5b796a0dc59
Zhiquan Lao <archer55190360@gmail.zom>
2024-07-22 00:33:53 -0400

    Finish creating animation

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -35,5 +35,31 @@ public class NBody{
         for(int i=0;i<planets.length;++i){
             planets[i].draw();
         }
+
+        //creating an animation
+        StdDraw.enableDoubleBuffering();
+        for(double cur_time=0;cur_time<=T;cur_time+=dt){
+            double [] xForce=new double[planets.length];
+            double [] yForce=new double[planets.length];
+            StdDraw.picture(0,0,"images/starfield.jpg");
+            for(int i=0;i<planets.length;++i){
+                xForce[i]=planets[i].calcNetForceExertedByX(planets);
+                yForce[i]=planets[i].calcNetForceExertedByY(planets);
+            }
+            for (int i = 0; i < planets.length; i++) {
+                planets[i].update(dt, xForce[i], yForce[i]);
+                planets[i].draw();
+            }
+            StdDraw.show();;
+            StdDraw.pause(10);
+            //System.out.printf("cur_time=%f, planets[0].xxPos=%f, yyPos=%f,",cur_time,planets[0].xxPos,planets[0].yyPos);
+        }
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                        planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                        planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
+        }
     }
 }
