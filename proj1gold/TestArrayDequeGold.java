d69ddd0d1aadf04e16287c70677d8fd3d6bcc384
Zhiquan Lao <archer55190360@gmail.zom>
2024-09-09 00:19:10 -0400

    proj1gold

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class TestArrayDequeGold {
+    @Test 
+    public TestStudentArrayDeque() {
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque();
+        ArrayDequeSolution<Integer> sol = new ArrayDequeSolution();
+        for
+    }
+}
