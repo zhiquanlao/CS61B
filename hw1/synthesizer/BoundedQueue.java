9b38920719697e68595f31ba2c9ebb410e4524db
Zhiquan Lao <archer55190360@gmail.zom>
2024-09-30 23:08:00 -0400

    hw1 style

--- hw1/synthesizer/BoundedQueue.java
+++ hw1/synthesizer/BoundedQueue.java
@@ -8,7 +8,7 @@ public interface BoundedQueue<T> extends Iterable<T> {
     default boolean isEmpty() {
         return fillCount() == 0;
     }       // is the buffer empty (fillCount equals zero)?
-    default boolean isFull(){
+    default boolean isFull() {
         return fillCount() == capacity();
     }        // is the buffer full (fillCount is same as capacity)?
 }
