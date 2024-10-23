4e6ec92af2bbb20adcdaef8fe54eb56d7b51fb0a
Zhiquan Lao <archer55190360@gmail.zom>
2024-10-22 23:27:40 -0400

    hw2 buggy version

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -9,88 +9,124 @@ public class Percolation {
 
     private int num_grid;
     private int num_open;
+    private WeightedQuickUnionUF sets;
+    private boolean [][] site;
+    // private class grid {
+    //     private boolean open;
+    //     private grid parent;
+    //     private int size;
+    //     public grid() {
+    //         open = false;
+    //         size = 1;
+    //     }
+    // }
     
-    private class grid {
-        private boolean open;
-        private grid parent;
-        private int size;
-        public grid() {
-            open = false;
-            size = 1;
-            parent = this;
-        }
-    }
+    // //connect the grid with another grid
+    // private void union(grid p, grid q) {
+    //     grid i = find(p);
+    //     grid j = find(q);
+    //     if(i == j) {
+    //         return;
+    //     }
+    //     if(i.size < j.size) {
+    //         i.parent = j;
+    //         j.size += i.size;
+    //     } else {
+    //         j.parent = i;
+    //         i.size += j.size;
+    //     }
+    // }
     
-    //connect the grid with another grid
-    private void union(grid p, grid q) {
-        grid i = find(p);
-        grid j = find(q);
-        if(i == j) {
-            return;
-        }
-        if(i.size < j.size) {
-            i.parent = j;
-            j.size += i.size;
-        } else {
-            j.parent = i;
-            i.size += j.size;
-        }
-    }
-    
-    //find the parent of the grid
-    private grid find(grid p) {
-        if (p.parent == p) {
-            return p;
-        }
-        p.parent = find(p.parent);
-        return p.parent;
-    }
+    // //find the parent of the grid
+    // private grid find(grid p) {
+    //     if (p.parent == p) {
+    //         return p;
+    //     }
+    //     p.parent = find(p.parent);
+    //     return p.parent;
+    // }
 
-    private boolean is_connected(grid p, grid q) {
-        return find(p) == find(q);
-    }
-    private grid[][] site;
-    private grid top;
-    private grid bottom;
+    // private boolean is_connected(grid p, grid q) {
+    //     return find(p) == find(q);
+    // }
+    // private grid[][] site;
+    // private grid top;
+    // private grid bottom;
     
     // create N-by-N grid, with all sites initially blocked
     public Percolation(int N) {
         if (N <= 0) {
             throw new IllegalArgumentException();
         }
-        site = new grid[N][N];
-        top = new grid();
-        top.open = true;
-        bottom = new grid();
-        bottom.open = true;
+        // site = new grid[N][N];
+        // for (int i = 0; i < N; i++) {
+        //     for (int j = 0; j < N; ++j){
+        //         site[i][j].parent = site[i][j];
+        //     }
+        // }
+        // top = new grid();
+        // top.parent = top;
+        // top.open = true;
+        // bottom = new grid();
+        // bottom.parent = bottom;
+        // bottom.open = true;
+
+        //initialize all sites to block
+        site = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; ++j){
+                site[i][j] = false;
+            }
+        }
+        //site[i][j] is denoted by i * N + j in sets,
+        //the Nth and N+1th are bottom and top
+        sets = new WeightedQuickUnionUF(N * N + 2);
         num_grid = N;
         num_open = 0;
     }
-    
+    //convert [row][col] to the index in sets
+    private int row_col_to_index(int row, int col) {
+        return row * num_grid + col;
+    }
+    //get the col of the index in sets. ind<N^2
+    private int col_of_index(int ind) {
+        return ind % num_grid;
+    }
+    //get rol from the index in sets. ind<N^2
+    private int row_of_index(int ind) {
+        return ind / num_grid;
+    }
     // open the site (row, col) if it is not open already
     public void open(int row, int col) {
         if (row < 0 || row >= num_grid || col < 0 || col >=num_grid) {
             throw new IndexOutOfBoundsException();
         }
 
-        site[row][col].open = true;
+        site[row][col] = true;
 
         //union the site to nearby site
         if (row == num_grid - 1) {
-            union(bottom, site[row][col]);
-        } else if (site[row + 1][col].open) {
-            union(site[row][col], site[row + 1][col]);
+            //if it is the last row, union it to the bottom
+            sets.union(row_col_to_index(row, col), num_grid * num_grid);
+        } else if (site[row + 1][col]) {
+            sets.union(row_col_to_index(row, col), row_col_to_index(row + 1, col));
+            //union(site[row][col], site[row + 1][col]);
         }
         if (row == 0) {
-            union(top, site[row][col]);
-        } else if (site[row - 1][col].open) {
-            union(site[row][col], site[row - 1][col]);
+            //if at top row, union it to top
+            sets.union(row_col_to_index(row, col), num_grid * num_grid + 1);
+            //union(top, site[row][col]);
+        } else if (site[row - 1][col]) {
+            sets.union(row_col_to_index(row, col), row_col_to_index(row - 1, col));
+            //union(site[row][col], site[row - 1][col]);
         }
-        if (col - 1 >= 0 && site[row][col - 1].open) {
-            union(site[row][col], site[row][col - 1]);
+        if (col - 1 >= 0 && site[row][col - 1]) {
+            sets.union(row_col_to_index(row, col), row_col_to_index(row, col - 1));
+            //union(site[row][col], site[row][col - 1]);
         }
-        if (col + 1 <=num_grid - 1 && site[row][col + 1].open) {
-            union(site[row][col], site[row][col + 1]);
+        if (col + 1 <=num_grid - 1 && site[row][col + 1]) {
+            sets.union(row_col_to_index(row, col), row_col_to_index(row, col + 1));
+            //union(site[row][col], site[row][col + 1]);
         }
         num_open += 1;
     }   
@@ -100,7 +136,7 @@ public class Percolation {
         if (row < 0 || row >= num_grid || col < 0 || col >=num_grid) {
             throw new IndexOutOfBoundsException();
         }
-        return site[row][col].open;
+        return site[row][col];
     }
 
     // is the site (row, col) full?
@@ -108,7 +144,7 @@ public class Percolation {
         if (row < 0 || row >= num_grid || col < 0 || col >=num_grid) {
             throw new IndexOutOfBoundsException();
         }
-        return is_connected(top, site[row][col]);
+        return sets.find(num_grid * num_grid + 1) == sets.find(row_col_to_index(row, col));
     } 
 
     // number of open sites
@@ -118,7 +154,8 @@ public class Percolation {
     
     // does the system percolate?
     public boolean percolates() {
-        return is_connected(top, bottom);
+        //return is_connected(top, bottom);
+        return sets.find(num_grid * num_grid + 1) == sets.find(num_grid * num_grid);
     }             
     public static void main(String[] args) {
 
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,30 +1,54 @@
 package hw2;
 
+import javax.net.ssl.StandardConstants;
+
 import edu.princeton.cs.introcs.StdRandom;
 
 import edu.princeton.cs.introcs.StdStats;
 
 public class PercolationStats {
+    private double[] stat;
+    private int time_sim;
+
+    //simulate the percolation
+    private double simulate(int N, Percolation perc) {
+        while (!perc.percolates()) {
+            int row = StdRandom.uniform(N);
+            int col = StdRandom.uniform(N);
+            if (perc.isOpen(row, col)) {
+                continue;
+            }
+            perc.open(row, col);
+        }
+        return ((double) perc.numberOfOpenSites()) / (N * N);
+    }
+
     // perform T independent experiments on an N-by-N grid
     public PercolationStats(int N, int T, PercolationFactory pf) {
+        stat = new double[N];
         if (N <= 0 || T <= 0) {
             throw new IllegalArgumentException();
+        }
+        time_sim = T;
+        for (int i = 0; i < T; i++) {
+            Percolation perc = pf.make(N);
+            stat[i] = simulate(N, perc);
         }
     }
     // sample mean of percolation threshold
     public double mean() {
-        return 0;
+        return StdStats.mean(stat, 0, time_sim);
     }   
     // sample standard deviation of percolation threshold                                       
     public double stddev() {
-        return 0;
+        return StdStats.stddev(stat, 0, time_sim);
     }  
     // low endpoint of 95% confidence interval                                       
     public double confidenceLow() {
-        return 0;
+        return mean() - 1.96*stddev()/Math.sqrt(time_sim);
     }
     // high endpoint of 95% confidence interval                                 
     public double confidenceHigh() {
-        return 0;
+        return mean() + 1.96*stddev()/Math.sqrt(time_sim);
     }
 }
