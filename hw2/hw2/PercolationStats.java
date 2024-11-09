dcf158a395bb566f21513f65ea647a472de5cda3
Zhiquan Lao <archer55190360@gmail.zom>
2024-11-08 22:10:29 -0500

    hw2

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -26,10 +26,13 @@ public class PercolationStats {
         if (N <= 0 || T <= 0) {
             throw new IllegalArgumentException();
         }
-        stat = new double[N];
+        stat = new double[T];
         time_sim = T;
         for (int i = 0; i < T; i++) {
             Percolation perc = pf.make(N);
+
+            //System.out.println(i);
+
             stat[i] = simulate(N, perc);
         }
     }
@@ -49,4 +52,9 @@ public class PercolationStats {
     public double confidenceHigh() {
         return mean() + 1.96 * stddev() / Math.sqrt(time_sim);
     }
+    // public static void main(String[] args) {
+    //     PercolationFactory pf = new PercolationFactory();
+    //     PercolationStats temp = new PercolationStats(64,150,pf);
+    //     System.out.print(temp.mean());
+    // }
 }
