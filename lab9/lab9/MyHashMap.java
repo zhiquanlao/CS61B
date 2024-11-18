dde02d2bbf2124edeaa0ef6a9fdd9d9d3286e642
Zhiquan Lao <archer55190360@gmail.zom>
2024-11-17 22:36:52 -0500

    lab9 hash

--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -53,19 +53,22 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return buckets[hash(key)].get(key);
     }
 
     /* Associates the specified value with the specified key in this map. */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        int index = hash(key);
+        size -= buckets[index].size;
+        buckets[index].put(key, value);
+        size += buckets[index].size;
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
